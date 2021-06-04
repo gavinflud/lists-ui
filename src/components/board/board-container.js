0f65cd598ec80cd8feb3a2d4d05792d1abd4df80
Gavin Flood <gavin@gavinflood.com>
2021-06-04 18:06:35 +0100

    Lists can now be dragged to be reordered.
    This is done using the react-beatiful-dnd package. Updates to the server-side are not implemented yet.

--- src/components/board/board-container.js
+++ src/components/board/board-container.js
@@ -29,7 +29,7 @@ const BoardContainer = () => {
 
     const getLists = () => {
       Api.getListsForBoard(parseInt(id))
-          .then(lists => setLists([{id: 1, name: 'Backlog'}, {id: 2, name: 'In Progress'}]));
+          .then(lists => setLists(lists));
     };
 
     if (user && id) {
@@ -38,8 +38,52 @@ const BoardContainer = () => {
     }
   }, [user, id]);
 
+  useEffect(() => {
+    // TODO: Update lists on server side when they change on client side
+  }, [lists]);
+
+  /**
+   * Reorder the items in a list after an item has moved.
+   *
+   * @param list the list to reorder
+   * @param startIndex the index the item started at
+   * @param endIndex the index the item moved to
+   * @returns Object[] the reordered list
+   */
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+
+  /**
+   * Called when the user stops dragging an item.
+   *
+   * @param result the result after dragging
+   */
+  const onDragEnd = result => {
+    const {source, destination} = result;
+
+    if (!destination) {
+      return;
+    }
+
+    // This just accounts for lists right now and the droppableId for each list will be the same
+    const sourceId = source.droppableId;
+    const destinationId = destination.droppableId;
+
+    // TODO: Add logic for when an item moves to a different droppable element
+    if (sourceId === destinationId) {
+      const reorderedLists = reorder(lists, source.index, destination.index);
+      reorderedLists.forEach((list, i) => list.priority = i);
+      setLists(reorderedLists);
+    }
+  };
+
   return <Board board={board}
-                lists={lists}/>;
+                lists={lists}
+                onDragEnd={onDragEnd}/>;
 
 };
 
--- src/components/board/board-view.js
+++ src/components/board/board-view.js
@@ -1,3 +1,4 @@
+import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
 import List from '../list/';
 import './board-view.scss';
 
@@ -6,17 +7,62 @@ import './board-view.scss';
  */
 const Board = (props) => {
 
+  /**
+   * Context-aware class name for a list.
+   *
+   * @param isDragging true if the list is currently being dragged, false otherwise
+   * @returns {string} the class to attach to the element
+   */
+  const getListClassName = isDragging => {
+    return isDragging ? 'gf-list-draggable gf-list-dragging' : 'gf-list-draggable';
+  };
+
+  /**
+   * Map a list to a Draggable component
+   *
+   * @param list the list to map
+   * @returns {JSX.Element} Draggable component created from the list
+   */
+  const toDraggable = list => {
+    return (
+        <Draggable key={list.id}
+                   draggableId={'list-' + list.id}
+                   index={list.priority}>
+          {(provided, snapshot) => (
+              <div ref={provided.innerRef}
+                   className={getListClassName(snapshot.isDragging)}
+                   {...provided.draggableProps}
+                   {...provided.dragHandleProps}>
+                <List list={list}/>
+              </div>
+          )}
+        </Draggable>
+    );
+  };
+
   return (
       <section className="section gf-board-view">
         <h1>{props.board.name}</h1>
 
-        <div className="gf-lists-container">
-          {props.lists.map(list => <List list={list}/>)}
+        <DragDropContext onDragEnd={props.onDragEnd}>
+          <Droppable droppableId="droppable-board" direction="horizontal">
+            {(provided, snapshot) => (
+                <div ref={provided.innerRef}
+                     className={snapshot.isDraggingOver
+                         ? 'gf-lists-container gf-lists-container-dragging'
+                         : 'gf-lists-container'}
+                     {...provided.droppableProps}>
+                  {props.lists.map(list => toDraggable(list))}
 
-          <div className="gf-list-container gf-list-container-button">
-            <button>New List</button>
-          </div>
-        </div>
+                  {provided.placeholder}
+
+                  <div className="gf-list-draggable gf-list-container-button">
+                    <button>New List</button>
+                  </div>
+                </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </section>
   );
 
